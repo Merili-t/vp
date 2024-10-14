95c1a11d2939b081e759059de59fe6bfe686dc80
Merili Tiik <meritiik@greeny.cs.tlu.ee>
2024-10-14 10:20:45 +0300

    kodutöö 07.okt

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const dateTime = require ("./dateTime");
 const fs = require("fs");
 const bodyparser = require("body-parser");
 const app = express();
+const path = require("path");
+
 
 //määran view mootori
 app.set("view engine", "ejs");
@@ -53,7 +55,10 @@ app.post("/regvisit", (req, res)=>{
 			throw err;
 		}
 		else {
-			fs.appendFile("public/textfiles/log.txt", req.body.firstNameInput + " " + req.body.lastNameInput + ";", (err) =>{
+			const now = new Date();
+            		const dateString = now.toLocaleString();
+			
+			fs.appendFile("public/textfiles/log.txt", req.body.firstNameInput + " " + req.body.lastNameInput + dateString + ";", (err) =>{
 				if (err){
 					throw err;
 				}
@@ -65,6 +70,21 @@ app.post("/regvisit", (req, res)=>{
 		}
 	});
 });
+
+
+app.get("/reg", (req, res) => {
+    const logFilePath = path.join(__dirname, "public/textfiles/log.txt");
+   
+    fs.readFile(logFilePath, "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+      
+        reg = data.split(";")        
+	res.render("reg", { reg: reg });
+    });
+});
+
 
 
 app.listen(5204);
